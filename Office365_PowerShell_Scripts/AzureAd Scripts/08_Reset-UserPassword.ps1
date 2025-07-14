c1fa4f00765e2fc0c9522900bf7e0df8e638d6d3
Ste Walsh <Ste.walsh123@hotmail.com>
2025-07-14 09:11:41 +0100

    Cleanup

--- Office365_PowerShell_Scripts/AzureAd Scripts/08_Reset-UserPassword.ps1
+++ Office365_PowerShell_Scripts/AzureAd Scripts/08_Reset-UserPassword.ps1
@@ -1,2 +1,2 @@
 # Reset a user's password forcing password change at next login
-Set-MsolUserPassword -UserPrincipalName john.doe@yourdomain.com -NewPassword "NewPassword123!" -ForceChangePassword $true
+Set-MsolUserPassword -UserPrincipalName john.doe@yourdomain.com -NewPassword "*" -ForceChangePassword $true
--- Office365_PowerShell_Scripts/AzureAd Scripts/29_BulkCreateUsersFromCSV.ps1
+++
@@ -1,4 +0,0 @@
-# Bulk create users from CSV file (expects UserPrincipalName, DisplayName columns)
-Import-Csv users.csv | ForEach-Object {
-    New-MsolUser -UserPrincipalName $_.UserPrincipalName -DisplayName $_.DisplayName -Password "DefaultPassword123" -ForceChangePassword $true
-}
